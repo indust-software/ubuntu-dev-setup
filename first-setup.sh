053ad5c06368f9146ad99f70913b0af77a353733
Michal Katanski <mike.katanski@gmail.com>
2015-11-06 20:10:45 +0100

    Create main script

---
+++ first-setup.sh
@@ -0,0 +1,11 @@
+# !/bin/bash
+
+
+
+# Default variables
+
+
+
+# Create welcome screen
+whiptail --title "Ubuntu Development Setup" \
+--msgbox "This script will configure your development environment. It will install most common applications and configure system to be ready for developers.\n\n Choose Ok to continue or press ESC key to abort." 13 60
